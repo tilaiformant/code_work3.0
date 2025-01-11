16b442718c1cf021e4155a76708791b94382b741
wujianyu <wujianyu322@gmail.com>
2025-01-11 16:34:03 +0800

    列印變數區是一個表達式

--- test10.c
+++ test10.c
@@ -1,3 +1,4 @@
+/*     ch3_4_1.c        */
 #include <stdio.h>
 
 int main()
@@ -6,7 +7,7 @@ int main()
 
     i = 3;
     j = 4;
-    printf("exercise ch%d_%d.c \n" ,i,j);
+    printf("i + j = %d\n",i + j);
 
     getchar();
     return 0;
