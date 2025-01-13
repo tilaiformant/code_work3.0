33ec538d079efe651835b6a3deeaee4fb10233e9
wujianyu <wujianyu322@gmail.com>
2025-01-13 14:44:54 +0800

    fflush()函數使用

--- test10.c
+++ test10.c
@@ -1,23 +1,18 @@
-/*     ch3_11.c        */
+/*    ch2_16.c    */
 #include <stdio.h>
 
 int main()
-{
-      int i;
-      char ch;
+{   
+    int i;
+    char ch;
 
-      printf("請輸入 1 個整數 \n==>");
-      scanf("%d",&i);
-      printf("請輸入 1 個字元 \n==>");
-      scanf(" %c",&ch);  /* 可跳過不可列印字元 */
-      printf("整數是=%d, Ascii碼值是=%d, 字元是=%c \n",i,ch,ch);
+    printf("請輸入 1 個整數 \n==>");
+    scanf("%d",&i);
+    printf("請輸入 1 個字元 \n==>");
+    fflush(stdin);   /* 清除緩衝區 */
+    scanf("%c",&ch);
+    printf("整數是=%d, Ascii碼值是=%d, 字元是=%c \n",i,ch,ch);
 
-    getchar();
+    getchar();// 等待使用者輸入一個字元（按任意鍵）
     return 0;
-
-
-
-
-
-
 }
