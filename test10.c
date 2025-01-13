0057f04d6a2ba84b7b6d500e88ffbc74ee73f71d
wujianyu <wujianyu322@gmail.com>
2025-01-13 13:29:22 +0800

    讀取8進位,10進位與16進位的數值資料

--- test10.c
+++ test10.c
@@ -3,9 +3,18 @@
 
 int main()
 {
-        printf("不尋常的輸出 %d\n", 1);
-        printf("ch%d_%d.c\n",3,15);
-        
+      int i,j,k;
+      printf("請輸入 10 進位數值 : ");
+      scanf("%d",&i);
+      printf("請輸入 8 進位數值 : ");
+      scanf("%o",&j);
+      printf("請輸入 16 進位數值 : ");
+      scanf("%x",&k);
+      printf("i = %d\n",i);
+      printf("j = %d\n",j);
+      printf("k = %d\n",k);
+      printf("i + j + k = %d\n",i + j + k);
+
        
 
      
