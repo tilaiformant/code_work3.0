697f20cb38f04962300cf61692b6fe443862393f
wujianyu <wujianyu322@gmail.com>
2025-01-13 10:20:01 +0800

    格式化輸出某一字元變數值得應用

--- test10.c
+++ test10.c
@@ -3,16 +3,11 @@
 
 int main()
 {
-    float i;
-
-    i = 123.56;
-    printf("/%f/\n",i);
-    printf("/%3.2f/\n",i);
-    printf("/%8.2f/\n",i);
-    printf("/%-8.2f/\n",i);
-    printf("/%+8.2f/\n",i);
-    printf("/%08.2f/\n",i);
-
+    char i;
+    i = 'a';
+    printf("/%c/\n",i);
+    printf("/%3c/\n",i);
+    printf("/%-3c/\n",i);
     getchar();
     return 0;
 
