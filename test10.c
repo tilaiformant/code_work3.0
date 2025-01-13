c4723743f057fdd1dcbbe1efd500029f834bfd29
wujianyu <wujianyu322@gmail.com>
2025-01-13 12:01:55 +0800

    格式化輸出其他類型變數的應用

--- test10.c
+++ test10.c
@@ -3,9 +3,21 @@
 
 int main()
 {
-    
-    printf("\104\105\105\120\n");
-    printf("\144\145\145\160\n");
+        int i = 10;
+        float j = 123.56;
+
+        printf("格式化輸出八位元\n");
+        printf("/%o/\n",i);
+        printf("/%-8o/\n",i);
+        printf("格式化輸出十六位元\n");
+        printf("/%x/\n",i);
+        printf("/%8x/\n",i);
+        printf("格式化輸出不帶正負號數值\n");
+        printf("/%u/\n",i);
+        printf("/%8u/\n",i);
+        printf("格式化輸出科學符號\n");
+        printf("/%e/\n",j);
+        printf("/%8.3e/\n",j);
 
      
 
