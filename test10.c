1955e1ee2dd0cfcd9f3b266e986688f06142f66b
wujianyu <wujianyu322@gmail.com>
2025-01-13 12:11:14 +0800

    不尋常的printf輸出

--- test10.c
+++ test10.c
@@ -3,21 +3,10 @@
 
 int main()
 {
-        int i = 10;
-        float j = 123.56;
-
-        printf("格式化輸出八位元\n");
-        printf("/%o/\n",i);
-        printf("/%-8o/\n",i);
-        printf("格式化輸出十六位元\n");
-        printf("/%x/\n",i);
-        printf("/%8x/\n",i);
-        printf("格式化輸出不帶正負號數值\n");
-        printf("/%u/\n",i);
-        printf("/%8u/\n",i);
-        printf("格式化輸出科學符號\n");
-        printf("/%e/\n",j);
-        printf("/%8.3e/\n",j);
+        printf("不尋常的輸出 %d\n", 1);
+        printf("ch%d_%d.c\n",3,15);
+        
+       
 
      
 
