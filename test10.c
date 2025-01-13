495ade5e9f3ae6b8b117ac599c91df6d77c158b5
wujianyu <wujianyu322@gmail.com>
2025-01-13 14:15:41 +0800

    用scanf()讀取資料

--- test10.c
+++ test10.c
@@ -3,12 +3,14 @@
 
 int main()
 {
-      int a, b;
+      int i;
+      char ch;
 
-      printf("請輸入兩個整數 : ");
-      scanf("%d %d",&a, &b);
-      printf("a + b = %d\n",a + b);
-       
+      printf("請輸入 1 個整數 \n==>");
+      scanf("%d",&i);
+      printf("請輸入 1 個字元 \n==>");
+      scanf(" %c",&ch);  /* 可跳過不可列印字元 */
+      printf("整數是=%d, Ascii碼值是=%d, 字元是=%c \n",i,ch,ch);
 
     getchar();
     return 0;
